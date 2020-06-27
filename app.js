e7458c2dd707b1766a16d2b0b8e9c26ffc729dc8
bslzz <bishalkoid@gmail.com>
2020-06-27 20:52:25 +0300

    created express test route and added files to gitignore

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+// .env keys
+require('dotenv').config();
+
+// express middleware
+app.use(express.json());
+
+//test app route
+app.get('/', (req, res) => {
+  res.json('👍 Hello from express route');
+});
+
+// PORT setup
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
