381919746b00ebcea0908490237d42ee110617e1
bslzz <bishalkoid@gmail.com>
2020-06-28 19:26:41 +0300

    routes created for frontend and styled

---
+++ client/src/components/AuthOptions.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+const AuthOptions = () => {
+  const history = useHistory();
+  const login = () => history.push('/login');
+  const register = () => history.push('/register');
+  return (
+    <div className="auth-buttons">
+      <button onClick={login}>Login</button>
+      <button onClick={register}>Register</button>
+    </div>
+  );
+};
+
+export default AuthOptions;
